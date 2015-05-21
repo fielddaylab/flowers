354e22db98e6b793f794d72416dfbd0a054c6258
Phildo <Phildo211@gmail.com>
2015-05-21 13:10:54 -0500

    drawing wind

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -2,32 +2,39 @@ var GamePlayScene = function(game, stage)
 {
   var self = this;
 
+  var dragger;
+
   var flowers = [];
   var bees = [];
   var hives = [];
-  var wind = [];
+  var wind;
 
   self.ready = function()
   {
+    dragger = new Dragger({source:stage.dispCanv.canvas});
+
     flowers.push(new Flower());
     for(var i = 0; i < 100; i++) bees.push(new Bee());
     hives.push(new Hive());
+    wind = new Wind(0,0,stage.dispCanv.canvas.width,stage.dispCanv.canvas.height);
+    dragger.register(wind);
   };
 
   self.tick = function()
   {
+    dragger.flush();
+    wind.tick();
     for(var i = 0; i < hives.length;   i++) hives[i].tick();
     for(var i = 0; i < flowers.length; i++) flowers[i].tick();
     for(var i = 0; i < bees.length;    i++) bees[i].tick();
-    for(var i = 0; i < wind.length;    i++) wind[i].tick();
   };
 
   self.draw = function()
   {
+    wind.draw(stage.drawCanv);
     for(var i = 0; i < hives.length;   i++) hives[i].draw(stage.drawCanv);
     for(var i = 0; i < flowers.length; i++) flowers[i].draw(stage.drawCanv);
     for(var i = 0; i < bees.length;    i++) bees[i].draw(stage.drawCanv);
-    for(var i = 0; i < wind.length;    i++) wind[i].draw(stage.drawCanv);
   };
 
   self.cleanup = function()
--- src/scenes/wind.js
+++ src/scenes/wind.js
@@ -1,15 +1,81 @@
-var Wind = function()
+var Gust = function()
 {
   var self = this;
 
+  self.strength = 100;
+  self.pts = [];
+
+  self.addPt = function(pt)
+  {
+    self.pts.push(pt);
+  }
+  self.lastPt = function()
+  {
+    return self.pts[self.pts.length-1];
+  }
+
   self.tick = function()
   {
+    self.strength--;
 
+    return self.strength;
   }
 
   self.draw = function(canv)
   {
+    canv.context.strokeStyle = "#000000";
+    canv.context.beginPath();
+    canv.context.moveTo(self.pts[0][0],self.pts[0][1]);
+    for(var i = 1; i < self.pts.length; i++)
+    {
+      canv.context.lineTo(self.pts[i][0],self.pts[i][1]);
+    }
+    canv.context.stroke();
+  }
+}
 
+var Wind = function(x,y,w,h)
+{
+  var self = this;
+  self.x = x;
+  self.y = y;
+  self.w = w;
+  self.h = h;
+
+  self.gusts = [];
+
+  self.tick = function()
+  {
+    for(var i = 0; i < self.gusts.length; i++)
+      self.gusts[i].tick();
+  }
+
+  self.draw = function(canv)
+  {
+    for(var i = 0; i < self.gusts.length; i++)
+      self.gusts[i].draw(canv);
+  }
+
+  self.dragging = false;
+  self.dragGust;
+  self.dragStart = function(evt)
+  {
+    self.dragging = true;
+    self.dragGust = new Gust();
+    self.dragGust.addPt([evt.doX,evt.doY]);
+    self.gusts.push(self.dragGust);
+  }
+  self.drag = function(evt)
+  {
+    var pt = self.dragGust.lastPt();
+    var x = Math.abs(pt[0]-evt.doX);
+    var y = Math.abs(pt[1]-evt.doY);
+    if(x*x + y*y > 100) //10 px apart
+      self.dragGust.addPt([evt.doX,evt.doY]);
+  }
+  self.dragFinish = function(evt)
+  {
+    self.dragging = false;
   }
 }
 
